261371d1b5f388cb1bc314df432b48b25c629532
Siesta1510 <93849920+Siesta1510@users.noreply.github.com>
2023-02-07 01:15:12 +0700

    making a server

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 import express from "express";
+import Discord, { Message, IntentsBitField } from "discord.js";
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res)=>{
-    res.send("HelloWorld")
-})
+const client = new Discord.Client({
+  intents: [
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+app.get("/", (req, res) => {
+  res.send("HelloWorld");
+});
 
-app.listen(5000, (req, res) =>{
-    console.log("Hello");
-})
+client.on("ready", () => {
+  console.log("aaaaaaa");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "aaa") {
+    message.reply('Hi')
+  }
+});
+
+client.login(
+  "MTA2NDk1NTEwNTAwNDIyNDUzMg.G1Ohtn.fUC8karQ7M5KKwBdbZVyZv686d8atsMrOCXDr4"
+);
